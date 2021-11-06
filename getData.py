9e0a070d0cbaf25cb61f789695b5191a0214bd4e
indiesoul2 <39783175+indiesoul2@users.noreply.github.com>
2021-11-06 20:42:40 +0900

    Update getData.py

--- getData.py
+++ getData.py
@@ -198,7 +198,7 @@ def get_overview(ticker):
     df = pd.json_normalize(response_json)
     #2021.10.17 'FullTimeEmployees' 삭제
     description_data = ['MarketCapitalization', 'Symbol', 'AssetType', 'Name', 'Description', 'Exchange', 'Currency', 'Country', 'Sector', \
-                        'Industry', 'Address', 'FiscalYearEnd', 'LatestQuarter', 'LastSplitFactor', 'LastSplitDate']
+                        'Industry', 'Address', 'FiscalYearEnd', 'LatestQuarter']
     profit_data = ['RevenueTTM', 'RevenuePerShareTTM', 'ProfitMargin','GrossProfitTTM', 'OperatingMarginTTM', 'EBITDA', \
                     'QuarterlyEarningsGrowthYOY', 'QuarterlyRevenueGrowthYOY']
     dividend_data = ['DividendPerShare', 'DividendYield', 'PayoutRatio', \
